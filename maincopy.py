67a3ca808f1a45a1f9a7ee6d224529ea48d5d944
[BakedTaiyaki093] <[h.m.r.t.15109@gmail.com]>
2025-05-11 20:21:50 +0900

    TyAppsLauncher

--- maincopy.py
+++ maincopy.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from functools import partial
 
 # バージョン情報
-Version = 1.2
+Version = 1.0
 GITHUB_REPO = "https://github.com/BakedTaiyaki093/TyAppsLauncher"
 VERSION_URL = "https://raw.githubusercontent.com/BakedTaiyaki093/TyAppsLauncher/main/Version.txt"
 UPDATE_URL = "https://github.com/BakedTaiyaki093/TyAppsLauncher/raw/refs/heads/main/releases/TyAppsLauncher_latest.zip"
